12fd163f9bf8ee6e449a5f0d1ae331199f2dcc91
Bokusoro <orhan.agayev201@gmail.com>
2024-01-07 13:48:32 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let search = document.querySelector(`.input`);
 let button = document.querySelector(`#submit`);
 let img = document.querySelector(`.error_img`);
-button.addEventListener (`click`, function() {
-  img.innerHTML = "";
-  let x = search.value;
-  if (typeof x == `number`) {
-    console.log(1) ;
-    img.innerHTML += `<img  class="error_img" src="https://http.cat/images/${x}.jpg" alt="Error 404">`;
+button.addEventListener(`click`, function () {
+  let x = Number(search.value);
+  if (typeof x == "number") {
+    console.log(search.value);
+    let str = `https://http.cat/images/${x}.jpg`;
+    img.setAttribute("src", str);
+    console.log(img.innerHTML);
   }
-})
-  
+});
